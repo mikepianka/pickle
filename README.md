729bbb462a97e06134f0a2ea8a6748d5a48cfde7
Mike Pianka <mpianka@mjels.com>
2023-03-25 16:34:49 -0400

    fix RGBA val output, add alpha float option

--- README.md
+++ README.md
@@ -4,12 +4,16 @@ Pickle is a little tool for picking pixels.
 
 ## Usage
 
-Provide a JPG or PNG filepath and the X,Y coordinates of a pixel within the image. Pickle reads the image and returns the pixel value as RGBA {uint8 uint8 uint8 float64}.
+Provide a JPG or PNG filepath and the X,Y coordinates of a pixel within the image. Pickle reads the image and can return the pixel value as either RGBA {uint8 uint8 uint8 uint8} or RGBa {uint8 uint8 uint8 float64}.
+
+### Arguments
 
 ```bash
 >>> pickle -h
 
 Usage of ./pickle:
+  -af
+        Optional: Return alpha value as float instead of int (default false)
   -path string
         Path to JPG or PNG file
   -x int
@@ -18,14 +22,32 @@ Usage of ./pickle:
         Y coordinate of the pixel
 ```
 
-```bash
->>> pickle -path=./examples/gophers.jpg -x=100 -y=200
+### Examples
 
-Pixel at (100, 200) has RGBA values of {90 217 254 1}
+```bash
+# color image with standard output
+>>> pickle -path=./examples/gophers.jpg -x=300 -y=130
+
+Pixel at (300, 130) has RGBA values of R: 90, G: 218, B: 255, A: 255
 ```
 
 ```bash
->>> pickle -path=./examples/gophers_grayscale.png -x=100 -y=200
+# color image with floating alpha output
+>>> pickle -path=./examples/gophers.jpg -x=300 -y=130 -af
 
-Pixel at (100, 200) has RGBA values of {184 184 184 1}
+Pixel at (300, 130) has RGBA values of R: 90, G: 218, B: 255, A: 1.000000
+```
+
+```bash
+# grayscale image with standard output
+>>> pickle -path=./examples/gophers_grayscale.png -x=300 -y=130
+
+Pixel at (300, 130) has RGBA values of R: 184, G: 184, B: 184, A: 255
+```
+
+```bash
+# grayscale image with floating alpha output
+>>> pickle -path=./examples/gophers_grayscale.png -x=300 -y=130 -af
+
+Pixel at (300, 130) has RGBA values of R: 184, G: 184, B: 184, A: 1.000000
 ```
--- main.go
+++ main.go
@@ -7,43 +7,51 @@ import (
 	"log"
 	"os"
 
+	"image/color"
 	_ "image/jpeg"
 	_ "image/png"
 )
 
-type RGBA struct {
-	r uint8
-	g uint8
-	b uint8
-	a float64
+type RGBa struct {
+	R uint8
+	G uint8
+	B uint8
+	A float64
 }
 
 type Config struct {
 	imagePath string
 	x         int
 	y         int
+	aFloat    bool
 }
 
 func main() {
-	config, err := cli()
+	conf, err := cli()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	img, err := loadImage(config.imagePath)
+	img, err := loadImage(conf.imagePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	rgba := getPixelValue(img, config.x, config.y)
+	rgba := getPixelValue(img, conf.x, conf.y)
 
-	fmt.Printf("Pixel at (%d, %d) has RGBA values of %v\n", config.x, config.y, rgba)
+	if conf.aFloat {
+		a := alphaIntToFloat(rgba.A)
+		fmt.Printf("Pixel at (%d, %d) has RGBA values of R: %d, G: %d, B: %d, A: %f\n", conf.x, conf.y, rgba.R, rgba.G, rgba.B, a)
+	} else {
+		fmt.Printf("Pixel at (%d, %d) has RGBA values of R: %d, G: %d, B: %d, A: %d\n", conf.x, conf.y, rgba.R, rgba.G, rgba.B, rgba.A)
+	}
 }
 
 func cli() (Config, error) {
 	imagePath := flag.String("path", "", "Path to JPG or PNG file")
 	x := flag.Int("x", 0, "X coordinate of the pixel")
 	y := flag.Int("y", 0, "Y coordinate of the pixel")
+	aFloat := flag.Bool("af", false, "Optional: Return alpha value as float instead of int (default false)")
 
 	flag.Parse()
 
@@ -51,6 +59,7 @@ func cli() (Config, error) {
 		imagePath: *imagePath,
 		x:         *x,
 		y:         *y,
+		aFloat:    *aFloat,
 	}
 
 	if *imagePath == "" {
@@ -75,15 +84,11 @@ func loadImage(path string) (image.Image, error) {
 	return img, nil
 }
 
-func getPixelValue(img image.Image, x, y int) RGBA {
+func getPixelValue(img image.Image, x, y int) color.NRGBA {
 	pixel := img.At(x, y)
-	r32, g32, b32, a32 := pixel.RGBA()
+	return color.NRGBAModel.Convert(pixel).(color.NRGBA)
+}
 
-	// convert 16 bit values to 8 bit
-	return RGBA{
-		r: uint8(r32 / 257),
-		g: uint8(g32 / 257),
-		b: uint8(b32 / 257),
-		a: float64(a32) / 65535.0,
-	}
+func alphaIntToFloat(alpha uint8) float64 {
+	return float64(alpha) / 255
 }
